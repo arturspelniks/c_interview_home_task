f5820fcf67a856b2a24f8e750a4542899e6850d4
arturspelniks <go.go.dolb@gmail.com>
2022-09-25 11:01:39 +0300

    Added initial API version

---
+++ plsql/national_id_number_api_pkg_s.sql
@@ -0,0 +1,11 @@
+create or replace PACKAGE national_id_number_api_pkg AS
+  FUNCTION validate(p_national_id_number VARCHAR2
+                   ,p_country_code       VARCHAR2) RETURN BOOLEAN;
+
+  FUNCTION get_birth_date(p_national_id_number VARCHAR2
+                         ,p_country_code       VARCHAR2) RETURN DATE;
+
+  FUNCTION get_gender(p_national_id_number VARCHAR2
+                     ,p_country_code       VARCHAR2) RETURN VARCHAR2;
+END national_id_number_api_pkg;
+/
